46c1d9aa9987df2b1f39d48cb8b1378198f1929f
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-02-22 03:18:18 +0700

    Update Grid.cs

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,9 +6,7 @@ public class Grid : MonoBehaviour
 {
     [SerializeField]
     private  Vector2 _gridSize;
-
     private Vector2 _gridOffset;
-
     [SerializeField]
     private Sprite _cellSprite;
     private Vector2 _cellSize;
