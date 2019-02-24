8bc617d51c30c3c6bde0893b1436f43967e95e12
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-02-25 01:05:45 +0700

    Added TextMesh Pro

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Grid : MonoBehaviour
 {
-    [SerializeField] private  Vector2 _gridSize;
+    [SerializeField] private Vector2 _gridSize;
     [SerializeField] private Sprite _cellSprite;
-   
-    private Vector2 _gridOffset; 
+
+    private Vector2 _gridOffset;
     private Vector2 _cellSize;
     private Vector2 _cellScale;
 
     void Start()
     {
         InitCells();
+        
     }
 
     void InitCells()
@@ -39,13 +41,25 @@ public class Grid : MonoBehaviour
             for (int col = 0; col < _gridSize.y; col++)
             {
                 Vector2 pos = new Vector2(col * _cellSize.x + _gridOffset.x + transform.position.x, row * _cellSize.y + _gridOffset.y + transform.position.y);
-               
+
                 GameObject cO = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
                 cO.GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-                
+
                 cO.transform.parent = transform;
             }
         }
         Destroy(cellObject);
+    }
+
+    public void Write(int X, int Y, string symbol, Color color)
+    {
+        TMP_Text tmpSymbol = GetComponent<TMP_Text>();
+        tmpSymbol.text = symbol;
+
+        Vector2 pos = new Vector2(X* _cellSize.x + _gridOffset.x , Y * _cellSize.y + _gridOffset.y );
+
+        Instantiate(tmpSymbol,pos,Quaternion.identity);
+        tmpSymbol.GetComponent<TMP_Text>().material.color = color;
+
     }
 }
