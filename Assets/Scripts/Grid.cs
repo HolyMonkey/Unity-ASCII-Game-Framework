ee0447f0f4eff00841a2ce82198abaab4bc7e415
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-02-26 23:00:26 +0700

    003 Write Symbol

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -11,18 +11,22 @@ public class Grid : MonoBehaviour
     private Vector2 _gridOffset;
     private Vector2 _cellSize;
     private Vector2 _cellScale;
+    private TextMeshPro _tmpSymbol;
+    private GameObject [,] grid;
 
     void Start()
     {
         InitCells();
-        
     }
 
     void InitCells()
     {
         GameObject cellObject = new GameObject();
 
+        grid = new GameObject[(int)_gridSize.x,(int) _gridSize.y];
         cellObject.AddComponent<SpriteRenderer>().sprite = _cellSprite;
+
+        cellObject.name = "Sprite";
 
         _cellSize = _cellSprite.bounds.size;
 
@@ -33,33 +37,42 @@ public class Grid : MonoBehaviour
 
         cellObject.transform.localScale = new Vector2(_cellScale.x, _cellScale.y);
 
-        _gridOffset.x = -(_gridSize.x / 2) + _cellSize.x / 2;
-        _gridOffset.y = -(_gridSize.y / 2) + _cellSize.y / 2;
+        
+        _gridOffset.x = -(_gridSize.x / 2)+ _cellSize.x / 2;
+        _gridOffset.y = (_gridSize.y / 2) - _cellSize.y / 2;
 
-        for (int row = 0; row < _gridSize.x; row++)
+        for (int row = 0; row < _gridSize.y; row++)
         {
-            for (int col = 0; col < _gridSize.y; col++)
+            for (int col = 0; col < _gridSize.x; col++)
             {
-                Vector2 pos = new Vector2(col * _cellSize.x + _gridOffset.x + transform.position.x, row * _cellSize.y + _gridOffset.y + transform.position.y);
-
-                GameObject cO = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
-                cO.GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-
-                cO.transform.parent = transform;
+                Vector2 pos = new Vector2(col * _cellSize.x + _gridOffset.x + transform.position.x, -row* _cellSize.y + _gridOffset.y + transform.position.y);
+                
+                grid[col, row] = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
+                grid[col, row].GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                
+                grid[col, row].transform.parent = transform;
             }
         }
+
         Destroy(cellObject);
     }
 
     public void Write(int X, int Y, string symbol, Color color)
     {
-        TMP_Text tmpSymbol = GetComponent<TMP_Text>();
-        tmpSymbol.text = symbol;
+        GameObject Symbol = new GameObject();
+        Symbol.name = "Symbol";
 
-        Vector2 pos = new Vector2(X* _cellSize.x + _gridOffset.x , Y * _cellSize.y + _gridOffset.y );
+        Symbol.AddComponent<TextMeshPro>().text = symbol;
+        TextMeshPro tmp = Symbol.GetComponent<TextMeshPro>();
 
-        Instantiate(tmpSymbol,pos,Quaternion.identity);
-        tmpSymbol.GetComponent<TMP_Text>().material.color = color;
+        tmp.rectTransform.sizeDelta= new Vector2(1,1.5f);
 
+        Symbol.transform.SetParent(grid[X, Y].transform);
+        Symbol.transform.localPosition= Vector2.zero;
+
+        tmp.enableAutoSizing = true;
+        tmp.color = color;
+        tmp.fontSizeMin = 0f;
+        tmp.alignment = TextAlignmentOptions.Center;
     }
 }
