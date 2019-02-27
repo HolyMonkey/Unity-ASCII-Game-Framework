9060310f03a5cc4911ae31575c6e3898188933a8
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-02-28 01:29:20 +0700

    Updated Grid
    _grid.SetCellSize(1);
    _grid.UpdateView(50, 50);
    
    Первый метод задаёт размер ячеек (1 юнит) второй создаёт чистую сетку 50х50 элементов

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,70 +5,58 @@ using TMPro;
 
 public class Grid : MonoBehaviour
 {
-    [SerializeField] private Vector2 _gridSize;
-    [SerializeField] private Sprite _cellSprite;
-
     private Vector2 _gridOffset;
-    private Vector2 _cellSize;
+    private Vector2 _cellSize=new Vector2(1,1);
     private Vector2 _cellScale;
-    private TextMeshPro _tmpSymbol;
-    private GameObject [,] grid;
+    private GameObject [,] _grid;
 
-    void Start()
+    public void SetCellSize(int cellSize)
     {
-        InitCells();
+        _cellSize = new Vector2(cellSize,cellSize);
     }
 
-    void InitCells()
+    public void UpdateView(int X, int Y)
     {
         GameObject cellObject = new GameObject();
-
-        grid = new GameObject[(int)_gridSize.x,(int) _gridSize.y];
-        cellObject.AddComponent<SpriteRenderer>().sprite = _cellSprite;
-
-        cellObject.name = "Sprite";
-
-        _cellSize = _cellSprite.bounds.size;
+        cellObject.name = "Cell";
+        
+        _grid = new GameObject[X,Y];
 
         _cellScale.x = 1 / _cellSize.x;
         _cellScale.y = 1 / _cellSize.y;
 
-        _cellSize = new Vector2(1, 1);
-
         cellObject.transform.localScale = new Vector2(_cellScale.x, _cellScale.y);
 
-        
-        _gridOffset.x = -(_gridSize.x / 2)+ _cellSize.x / 2;
-        _gridOffset.y = (_gridSize.y / 2) - _cellSize.y / 2;
+        _gridOffset.x = -(X/ 2) + _cellSize.x / 2;
+        _gridOffset.y = (Y / 2) - _cellSize.y / 2;
 
-        for (int row = 0; row < _gridSize.y; row++)
+        for (int row = 0; row < Y; row++)
         {
-            for (int col = 0; col < _gridSize.x; col++)
+            for (int col = 0; col <X; col++)
             {
-                Vector2 pos = new Vector2(col * _cellSize.x + _gridOffset.x + transform.position.x, -row* _cellSize.y + _gridOffset.y + transform.position.y);
-                
-                grid[col, row] = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
-                grid[col, row].GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-                
-                grid[col, row].transform.parent = transform;
+                Vector2 pos = new Vector2(col * _cellSize.x + _gridOffset.x, -row * _cellSize.y + _gridOffset.y);
+
+                _grid[col, row] = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
+                _grid[col, row].transform.parent = gameObject.transform;
             }
         }
 
         Destroy(cellObject);
+
     }
 
     public void Write(int X, int Y, string symbol, Color color)
     {
-        GameObject Symbol = new GameObject();
-        Symbol.name = "Symbol";
+        GameObject cellSymbol = new GameObject();
+        cellSymbol.name = "Symbol";
 
-        Symbol.AddComponent<TextMeshPro>().text = symbol;
-        TextMeshPro tmp = Symbol.GetComponent<TextMeshPro>();
+        cellSymbol.AddComponent<TextMeshPro>().text = symbol;
+        TextMeshPro tmp = cellSymbol.GetComponent<TextMeshPro>();
 
-        tmp.rectTransform.sizeDelta= new Vector2(1,1.5f);
+        tmp.rectTransform.sizeDelta= new Vector2(_cellSize.x,_cellSize.y*1.5f);
 
-        Symbol.transform.SetParent(grid[X, Y].transform);
-        Symbol.transform.localPosition= Vector2.zero;
+        cellSymbol.transform.SetParent(_grid[X, Y].transform);
+        cellSymbol.transform.localPosition= Vector2.zero;
 
         tmp.enableAutoSizing = true;
         tmp.color = color;
