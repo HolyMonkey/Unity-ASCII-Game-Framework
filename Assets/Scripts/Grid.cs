4bde5d9a8e96bddacdafb75414e25f74d1618a16
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-02-28 21:41:14 +0700

    006 Added GridCamera

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,10 +5,18 @@ using TMPro;
 
 public class Grid : MonoBehaviour
 {
+    [Tooltip ("Add gird with GridCamera component")]
+    public GridCamera gridCam;
+
     private Vector2 _gridOffset;
     private Vector2 _cellSize=new Vector2(1,1);
     private Vector2 _cellScale;
     private GameObject [,] _grid;
+
+    public int cellSize
+    {
+        get { return (int)_cellSize.x; }
+    }
 
     public void SetCellSize(int cellSize)
     {
---
+++ Assets/Scripts/GridCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCamera : MonoBehaviour
+{
+    public Grid workGrid;
+    private Vector3 _currentPos;
+
+    void Start()
+    {
+        Camera.main.orthographic = true;
+        _currentPos = Camera.main.transform.position;
+    }
+
+    private void ChangePosition(Vector3 newPos,ref Vector3 _currentPos)
+    {
+        
+        Camera.main.transform.position = newPos;
+        _currentPos = Camera.main.transform.position;
+    }
+
+    public void MoveUp()
+    {
+        ChangePosition(new Vector3(_currentPos.x, _currentPos.y + workGrid.cellSize,_currentPos.z), ref _currentPos);
+    }
+
+    public void MoveDown()
+    {
+        ChangePosition(new Vector3(_currentPos.x, _currentPos.y - workGrid.cellSize, _currentPos.z), ref _currentPos);
+    }
+
+    public void MoveLeft()
+    {
+        ChangePosition(new Vector3(_currentPos.x - workGrid.cellSize, _currentPos.y, _currentPos.z), ref _currentPos);
+    }
+
+    public void MoveRight()
+    {
+        ChangePosition(new Vector3(_currentPos.x + workGrid.cellSize, _currentPos.y, _currentPos.z), ref _currentPos);
+    }
+
+}
