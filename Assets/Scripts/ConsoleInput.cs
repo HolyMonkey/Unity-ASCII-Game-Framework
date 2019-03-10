8bc1a8d853b0e25b743dad75b31aa2eb90cec733
Юлия Александрова <47824297+isharemycake@users.noreply.github.com>
2019-03-10 17:43:34 +0700

    007 InputFIeld

---
+++ Assets/Scripts/ConsoleInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ConsoleInput : MonoBehaviour
+{
+    public GameObject InputField;
+    public delegate void GetFunc (string input);
+
+    private GameObject _inputField;
+
+    public void ShowstringInputField(GetFunc OnEnter)
+    {
+        _inputField = Instantiate(InputField);
+        _inputField.GetComponentInChildren<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
+
+        _inputField.GetComponentInChildren<TMP_InputField>().onEndEdit.AddListener(delegate { OnEnter(_inputField.GetComponentInChildren<TMP_InputField>().text); Destroy(_inputField); });
+    }
+}
