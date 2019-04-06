2d3560bb6b0b74f8e83d98579cf136f17c3922ca
Саша Волков <Lexvolk2004@yandex.ru>
2019-04-07 00:32:46 +0400

    Level initialization

--- Assets/Scripts/PacmanGame.cs
+++ Assets/Scripts/PacmanGame.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PacmanGame : MonoBehaviour
 {
+    [SerializeField] private Grid _grid;
 
+    private Level _level;
+    private char _food = '·';
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        string[] map = File.ReadAllLines("Assets/Resources/PacmanLvl.txt");
+        _level = new Level(_grid, map);
+        _level.GenerationRule += (symbol) =>
+        {
+            if (symbol == ' ')
+                return _food;
+            else
+                return symbol;
+        };
     }
 
     // Update is called once per frame
